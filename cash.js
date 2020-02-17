43108f6c1c27bce8895949d1b0c641232696b8dd
Ramesh21071993 <40740112+Ramesh21071993@users.noreply.github.com>
2020-02-17 18:21:41 +0530

    Update cash.js

--- cash.js
+++ cash.js
@@ -1,29 +1,31 @@
-
-var target_amount = 99;
-var denominations = [1, 5, 10];
+var target_amount = 3;
+var denominations = [1];
 var result = 0;
 var denominations_count = [];
 var total_coins = getChange(target_amount, denominations);
-console.log(total_coins, denominations_count)
-
-
+console.log(total_coins, denominations_count);
 
 function getChange(target_amount, denominations) {
-    if(target_amount == 0) {
-        return 0;
-    }
-    var maxNumber = findMaxNumber(target_amount, denominations);
-    var quotient = Math.floor(target_amount/maxNumber);
-    denominations_count.push(`${quotient}x${maxNumber}`)
-    result = (parseInt(result)+parseInt(quotient));
+  if (target_amount == 0) {
+    return 0;
+  }
+  var maxNumber = findMaxNumber(target_amount, denominations);
+  if (maxNumber) {
+    var quotient = Math.floor(target_amount / maxNumber);
+    denominations_count.push(`${quotient}x${maxNumber}`);
+    result = parseInt(result) + parseInt(quotient);
     var remainder = target_amount % maxNumber;
-    if(remainder>0) {
-        getChange(remainder, denominations)
+    if (remainder > 0) {
+      getChange(remainder, denominations);
     }
-    return result;
+  }
+  return result;
 }
 
 function findMaxNumber(target_amount, denominations) {
+  if (denominations.length == 0) {
+    return false;
+  }
   let denominationsCopy = denominations;
   var maxNumber = Math.max.apply(Math, denominationsCopy);
   if (maxNumber > target_amount) {
